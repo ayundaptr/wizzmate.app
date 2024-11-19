c8791b49362fc87354a13d5761ff6f88674fa7eb
Indra Setya A.I <irwanblora286@gmail.com>
2024-11-19 23:23:26 +0700

    first commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,30 +1,18 @@
 // firebase-config.js
-const { initializeApp } = require("firebase/app"); // Modular import
-const {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} = require("firebase/auth"); // Modular import untuk auth
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
 
-// Konfigurasi Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT_ID.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID",
 };
 
-// Inisialisasi Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inisialisasi Firebase Auth
 const auth = getAuth(app);
 
-module.exports = {
-  auth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-};
+module.exports = { auth };
